e010d273d73b16924417ac1195f19ad054c30f0c
fffweb <fffweb@hotmail.com>
2017-03-08 15:38:49 +0800

    step 2

---
+++ angular2youtube/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: `my-app`,
+    template: `<h1>hellow world </h1>`
+})
+
+/**
+ * AppComponent
+ */
+export class AppComponent {
+    constructor() {
+        
+    }
+}
